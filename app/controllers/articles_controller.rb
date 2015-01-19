d702ec08c3081c78d58d70b4720642b6eb51ca6d
Ryan <ryanpauley@gmail.com>
2015-01-19 22:04:06 +1100

    cruddy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,40 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-	
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to article_path(@article)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+		else
+			render :edit
+		end
+		redirect_to @article
+
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :content, :tags)
+	end
+
 end
