0c95c410e9e64dcc3fb7b39a333aab70e48f9401
Ryan <ryanpauley@gmail.com>
2015-01-19 20:00:45 +1100

    First Post in Seeds. Render to Index and Show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+firstArticle = Article.create({
+	title: "Hello World",
+	content: "This is the first post on this new blog. <strong>Such Shiny!</strong> <em>So Content!</em> Much Style! <strong>Very Websiting!</strong> <br><br>You get the idea. <br><br>What you probably didn't know is that this is merely a <em>seedfile</em> to check my set up. I'd say don't read it, but you've already made it this far. So, as that's the case, I promise to give you a gif at the end. <img src='http://www.unocero.com/wp-content/uploads/2014/06/typing-fast-man.gif'>",
+	tags: "firstpost, rubyonrails, website, doge-like, helloworld"
+	});
